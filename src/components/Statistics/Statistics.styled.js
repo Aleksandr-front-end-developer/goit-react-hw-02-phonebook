a283f926f8d170477a596510e0c7b2cd5237f971
Oleksandr Danyliuk <oleksandr.danyliuk.88@gmail.com>
2024-01-10 10:49:27 +0200

    new

--- src/components/Statistics/Statistics.styled.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-export const Wrapper = styled.div`
-  font-size: 18px;
-  line-height: 1.3;
-  margin-top: 100px;
-  padding-right: 32px;
-  padding-left: 32px;
-`;
